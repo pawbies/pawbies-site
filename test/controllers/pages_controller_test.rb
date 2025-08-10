47007a862fd509909eb2bf7dacdea63eaf23f7cf
Pawbies <alex.fuchs970@gmail.com>
2025-08-10 21:39:33 +0200

    tests

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,28 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get pages_home_url
+  test "should get homepage" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get about_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+  end
+
+  test "should get privacy" do
+    get privacy_url
+    assert_response :success
+  end
+
+  test "should get imprint" do
+    get imprint_url
     assert_response :success
   end
 end
