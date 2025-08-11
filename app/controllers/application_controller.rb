90085adbc83ad019948052abcd1b578c49d792a0
Pawbies <alex.fuchs970@gmail.com>
2025-08-11 13:29:27 +0200

    optimizations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,33 @@ class ApplicationController < ActionController::Base
 
   around_action :switch_locale
 
-  def default_url_options
-    { locale: I18n.locale }
-  end
-
   private
     def switch_locale(&action)
-      locale = params[:locale] || I18n.default_locale
-      I18n.with_locale(locale, &action)
+      I18n.with_locale(current_locale, &action)
+    end
+
+    def current_locale
+      if params[:locale].present? && locale_available?(params[:locale])
+        save_locale_cookie(params[:locale])
+      elsif cookies.encrypted[:locale].present? && locale_available?(cookies.encrypted[:locale])
+        cookies.encrypted[:locale]
+      else
+        I18n.default_locale
+      end
+    end
+
+    def save_locale_cookie(locale)
+      cookies.encrypted[:locale] = {
+        value: locale.to_s,
+        expires: 1.year.from_now,
+        secure: Rails.env.production?,
+        same_site: :lax,
+        httponly: true
+      }
+      locale.to_s
+    end
+
+    def locale_available?(locale)
+      I18n.available_locales.map(&:to_s).include?(locale.to_s)
     end
 end
