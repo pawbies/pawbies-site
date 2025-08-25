eadb29803ca7b95d495ffbc496dcd2301578e49f
Pawbies <alex.fuchs970@gmail.com>
2025-08-25 16:42:37 +0200

    sender mail in tests

--- test/mailers/email_verifications_mailer_test.rb
+++ test/mailers/email_verifications_mailer_test.rb
@@ -7,7 +7,7 @@ class EmailVerificationsMailerTest < ActionMailer::TestCase
 
     assert_equal "Verify your email", mail.subject
     assert_equal [ user.email_address ], mail.to
-    assert_equal [ "noreply@pawbies.net" ], mail.from
+    assert_equal [ "messages@pawbies.net" ], mail.from
     assert_match "Hello #{user.full_name}, you can verify your email", mail.body.encoded
   end
 end
--- test/mailers/passwords_mailer_test.rb
+++ test/mailers/passwords_mailer_test.rb
@@ -7,7 +7,7 @@ class PasswordsMailerTest < ActionMailer::TestCase
 
     assert_equal "Reset your password", mail.subject
     assert_equal [ user.email_address ], mail.to
-    assert_equal [ "noreply@pawbies.net" ], mail.from
+    assert_equal [ "messages@pawbies.net" ], mail.from
     assert_match "You can reset your password within the next 15 minutes", mail.body.encoded
   end
 end
